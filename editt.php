74754c7e8448323f3fdc835b7a8d150357fa7759
prabniro <105201729+prabniro@users.noreply.github.com>
2022-05-14 14:23:57 +0545

    Update editt.php

--- editt.php
+++ editt.php
@@ -6,14 +6,14 @@ if(!$conn){
 }
 $sql ="UPDATE products set productid='$_POST[productid]',
                 productname='$_POST[Productname]',
-                Description='$_POST[Description]',
-                $price = '$_POST[price]';
-            $quantity  = '$_POST[quantity available]';
-            $productdescription = '$_POST[productdescription]';
+                description='$_POST[description]',
+                price = '$_POST[price]';
+            quantity  = '$_POST[quantity available]';
+            productdescription = '$_POST[productdescription]';
                  where id=$_POST[id]";
-$result = mysqli_query($conn,$sql);
+      $result = mysqli_query($conn,$sql);
 if($result){
-    header("Location:admin.php");
+    header("Location:products.php");
 }else{
 echo "Failed udpatig data" .mysqli_error($conn);
 }
